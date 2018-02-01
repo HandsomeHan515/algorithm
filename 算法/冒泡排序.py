ea258d095a83e148a750cb5de1ebed6967e48c51
HandsomeHan <HandsomeHan515@163.com>
2018-02-01 09:33:04 +0800

    Update 冒泡排序.py

--- 算法/冒泡排序.py
+++ 算法/冒泡排序.py
@@ -1,11 +1,17 @@
-arr = [10, 8, 4, 7, 5]
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-for i in range(len(arr) - 1):
-    for j in range(len(arr) - 1 - i):
-        if arr[j] > arr[j + 1]:
-            arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-
+                
+if __name__ == '__main__':
+    arr = [10, 8, 4, 7, 5]
+    sort_arr = bubble_sort(arr)
+    print(sort_arr)
+    
+# 排序过程   
 # [8, 4, 7, 5, 10]
 # [4, 7, 5, 8, 10]
 # [4, 5, 7, 8, 10]
