c046ec06b9ce5fd5e07c961e73c5d8a93525adae
HandsomeHan <HandsomeHan515@163.com>
2018-02-01 09:35:01 +0800

    Update 冒泡排序.py

--- 算法/冒泡排序.py
+++ 算法/冒泡排序.py
@@ -3,6 +3,7 @@ def bubble_sort(arr):
         for j in range(len(arr) - 1 - i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
 
                 
